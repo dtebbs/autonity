830804081a3f2a7946875815ee912bcf72b93fd5
Aneeque Safdar <as@clearmatics.com>
2021-07-14 13:40:37 +0100

    add parallel benchmark test

--- crypto/bls/blst/bls_benchmark_test.go
+++ crypto/bls/blst/bls_benchmark_test.go
@@ -8,6 +8,9 @@ import (
 
 // The following tutorial was used to base this benchmarking suite:
 // https://dave.cheney.net/2013/06/30/how-to-write-benchmarks-in-go
+// https://pkg.go.dev/testing
+// https://splice.com/blog/lesser-known-features-go-test/
+
 var preventCompilerOptimisationAggSig bls.BLSSignature
 var preventCompilerOptimisationVerifyResult bool
 
@@ -58,10 +61,28 @@ func benchmarkNAggregateSignatureVerifyFromMPKs(b *testing.B, n, m int) {
 	preventCompilerOptimisationVerifyResult = verifyR
 }
 
+func benchmarkNAggregateSignatureVerifyFromMPKsParallel(b *testing.B, n, m int) {
+	var verifyR bool
+	_, pks, sigs, msgs := genNMsgSignaturesFromMPks(b, n, m)
+	aggSig := AggregateSignatures(sigs)
+
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			verifyR = aggSig.AggregateVerify(pks, msgs)
+			if !verifyR {
+				b.Fatal(verifyR)
+			}
+		}
+	})
+	preventCompilerOptimisationVerifyResult = verifyR
+}
+
 // The final benchmark is only run once and that is not statically insignificant, however, the -benchtime flag can be
 // used to increase the total number of times the critical piece of code is run. An example command for running
-// benchmark is: go test -v -run=Bench -bench=. -benchtime=20s. This will ensure the minimum limit of execution is 20s.
-// Increase the benchtime flag to get more statisticall significant results.
+// benchmark is: go test -v -run=Bench -bench=. -benchtime=20s -cpu=1,2,4. This will ensure the minimum limit of
+// execution is 20s. Increase the benchtime flag to get more statisticall significant results.
 func Benchmark1AggregateSignatureFrom1PKs(b *testing.B) {
 	benchmarkNAggregateSignatureFromMPKs(b, 1, 1)
 }
@@ -75,16 +96,16 @@ func Benchmark10000DifferentAggregateSignatureFrom10000PKs(b *testing.B) {
 	benchmarkNAggregateSignatureFromMPKs(b, 10000, 1000)
 }
 
-func Benchmark1AggregateSignatureSignatureFrom1PKs(b *testing.B) {
+func Benchmark1AggregateSignatureSignatureVerifyFrom1PKs(b *testing.B) {
 	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1, 1)
 }
-func Benchmark100DifferentAggregateSignatureSignatureFrom100PKs(b *testing.B) {
+func Benchmark100DifferentAggregateSignatureVerifyFrom100PKs(b *testing.B) {
 	benchmarkNAggregateSignatureVerifyFromMPKs(b, 100, 100)
 }
-func Benchmark1000DifferentAggregateSignatureSignatureFrom1000PKs(b *testing.B) {
+func Benchmark1000DifferentAggregateSignatureVerifyFrom1000PKs(b *testing.B) {
 	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1000, 1000)
 }
-func Benchmark10000DifferentAggregateSignatureSignatureFrom10000PKs(b *testing.B) {
+func Benchmark10000DifferentAggregateSignatureVerifyFrom10000PKs(b *testing.B) {
 	benchmarkNAggregateSignatureVerifyFromMPKs(b, 10000, 10000)
 }
 
@@ -92,17 +113,10 @@ func Benchmark1000DifferentAggregateSignatureFrom100PKs(b *testing.B) {
 	benchmarkNAggregateSignatureFromMPKs(b, 1000, 100)
 }
 
-func Benchmark1000DifferentAggregateSignatureSignatureFrom100PKs(b *testing.B) {
+func Benchmark1000DifferentAggregateSignatureVerifyFrom100PKs(b *testing.B) {
 	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1000, 100)
 }
 
-func BenchmarkTest(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchmarkNAggregateSignatureVerifyFromMPKs(b, 100, 100)
-		benchmarkNAggregateSignatureVerifyFromMPKs(b, 100, 100)
-	}
-	//for i := 0; i < b.N; i++ {
-	//	benchmarkNAggregateSignatureVerifyFromMPKs(b, 100,100)
-	//	benchmarkNAggregateSignatureVerifyFromMPKs(b, 100,100)
-	//}
+func Benchmark1000DifferentAggregateSignatureVerifyFrom100PKsParallel(b *testing.B) {
+	benchmarkNAggregateSignatureVerifyFromMPKsParallel(b, 1000, 100)
 }
