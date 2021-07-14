fbf67d83521750e1ad6261fc3254b0b16ec8b3a4
Aneeque Safdar <as@clearmatics.com>
2021-07-14 17:41:09 +0100

    tabulate benchmarks

--- crypto/bls/blst/bls_benchmark_test.go
+++ crypto/bls/blst/bls_benchmark_test.go
@@ -1,6 +1,7 @@
 package blst
 
 import (
+	"fmt"
 	bls "github.com/clearmatics/autonity/crypto/bls/common"
 	"math/rand"
 	"testing"
@@ -11,32 +12,41 @@ import (
 // https://pkg.go.dev/testing
 // https://splice.com/blog/lesser-known-features-go-test/
 
+// The -benchtime flag can be used to increase the total number of times the critical piece of code is run. An example
+// command for running benchmark is: go test -v -run=Bench -bench=. -benchtime=20s -cpu=1,2,4. This will ensure the
+// minimum limit of execution is 20s. Increase the benchtime flag to get more statisticall significant results.
+
 var preventCompilerOptimisationVerifyResult bool
 
-func genNMsgSignaturesFromMPks(b *testing.B, n, m int) ([]bls.BLSSecretKey, []bls.BLSPublicKey, []bls.BLSSignature, [][32]byte) {
+func genNMsgSignaturesFromMPks(b *testing.B, numOfMsgs, numOfSigners int) ([]bls.BLSSecretKey, []bls.BLSPublicKey, []bls.BLSSignature, [][32]byte) {
 	var sigs []bls.BLSSignature
 	var privK []bls.BLSSecretKey
 	var pubK []bls.BLSPublicKey
 	var msgs [][32]byte
-	sks, pks, err := GenerateValidators(m)
-	for i := 0; i < n; i++ {
+
+	sks, pks, err := GenerateValidators(numOfSigners)
+
+	for i := 0; i < numOfMsgs; i++ {
 		msg := Msg{H: rand.Uint64(), R: rand.Uint64(), S: uint8(rand.Intn(3))}
 		if err != nil {
 			b.Fatal(err.Error())
 		}
+
 		msgB := msg.hash()
-		signerPrivK, signerPubK := sks[i%m], pks[i%m]
+		signerPrivK, signerPubK := sks[i%numOfSigners], pks[i%numOfSigners]
+
 		sigs = append(sigs, signerPrivK.Sign(msgB.Bytes()))
 		privK = append(privK, signerPrivK)
 		pubK = append(pubK, signerPubK)
 		msgs = append(msgs, msgB)
 	}
+
 	return privK, pubK, sigs, msgs
 }
 
-func benchmarkNAggregateSignatureVerifyFromMPKs(b *testing.B, n, m int) {
+func benchmarkAggregateNSignatureFromMPKsAndVerify(b *testing.B, numOfMsgs, numOfSigners int) {
 	var verifyR bool
-	_, pks, sigs, msgs := genNMsgSignaturesFromMPks(b, n, m)
+	_, pks, sigs, msgs := genNMsgSignaturesFromMPks(b, numOfMsgs, numOfSigners)
 	aggSig := AggregateSignatures(sigs)
 
 	b.ResetTimer()
@@ -49,53 +59,36 @@ func benchmarkNAggregateSignatureVerifyFromMPKs(b *testing.B, n, m int) {
 	preventCompilerOptimisationVerifyResult = verifyR
 }
 
-func benchmarkNAggregateSignatureVerifyFromMPKsParallel(b *testing.B, n, m int) {
-	var verifyR bool
-	_, pks, sigs, msgs := genNMsgSignaturesFromMPks(b, n, m)
-	aggSig := AggregateSignatures(sigs)
-
-	b.ResetTimer()
-
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			verifyR = aggSig.AggregateVerify(pks, msgs)
-			if !verifyR {
-				b.Fatal(verifyR)
-			}
-		}
-	})
-	preventCompilerOptimisationVerifyResult = verifyR
-}
-
-// The final benchmark is only run once and that is not statically insignificant, however, the -benchtime flag can be
-// used to increase the total number of times the critical piece of code is run. An example command for running
-// benchmark is: go test -v -run=Bench -bench=. -benchtime=20s -cpu=1,2,4. This will ensure the minimum limit of
-// execution is 20s. Increase the benchtime flag to get more statisticall significant results.
-func Benchmark1AggregateSignatureSignatureVerifyFrom1PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1, 1)
-}
-func Benchmark100DifferentAggregateSignatureVerifyFrom100PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 100, 100)
-}
-func Benchmark1000DifferentAggregateSignatureVerifyFrom1000PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1000, 1000)
-}
-func Benchmark10000DifferentAggregateSignatureVerifyFrom10000PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 10000, 10000)
-}
-
-func Benchmark1000DifferentAggregateSignatureVerifyFrom100PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1000, 100)
-}
-
-func Benchmark100000DifferentAggregateSignatureVerifyFrom100PKs(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 100000, 100)
-}
-func Benchmark1000DifferentAggregateSignatureVerifyFrom100PKsParallel(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKsParallel(b, 1000, 100)
-}
-
-// Horizontal Signature Aggregation: where 1 validator is signing multiple messages
-func Benchmark1000DifferentAggregateSignaruesVerifyFrom1PK(b *testing.B) {
-	benchmarkNAggregateSignatureVerifyFromMPKs(b, 1000, 1)
+func BenchmarkAggregateNSignatureFromMPKsAndVerify(b *testing.B) {
+	bms := []struct {
+		numOfMsgs    int
+		numOfSigners int
+	}{
+		{1000, 10},
+		{1000, 100},
+		{1000, 1000},
+		{10000, 10},
+		{10000, 100},
+		{10000, 1000},
+		{10000, 10000},
+		{100000, 10},
+		{100000, 100},
+		{100000, 1000},
+		{100000, 10000},
+		{100000, 100000},
+		{10, 1000},
+		{100, 1000},
+		{10, 10000},
+		{100, 10000},
+		{1000, 10000},
+		{10, 100000},
+		{100, 100000},
+		{1000, 100000},
+		{10000, 100000},
+	}
+	for _, bm := range bms {
+		b.Run(fmt.Sprintf("%v messages signed by %v private keys", bm.numOfMsgs, bm.numOfSigners), func(b *testing.B) {
+			benchmarkAggregateNSignatureFromMPKsAndVerify(b, bm.numOfMsgs, bm.numOfSigners)
+		})
+	}
 }
